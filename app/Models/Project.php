1be748b7f82fc535bcd607a24e007bc0e4fd3f67
aletardio <ale.tardio99@gmail.com>
2024-02-27 15:47:27 +0100

    Added function in each model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'link', 'slug', 'cover_image', 'description'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
